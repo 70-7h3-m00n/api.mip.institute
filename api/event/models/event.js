e17a5445a48fb27e25e8cc099bd803a4dd44141d
Ivan Kolpakov <vanjaklp95@gmail.com>
2023-12-06 18:27:07 +0300

    events model added

--- api/event/models/event.js
+++ api/event/models/event.js
@@ -4,12 +4,10 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
-
-const slugify = require('slugify')
-
+ const slugify = require('slugify')
 
 module.exports = {
-  lifecycles: {
+    lifecycles: {
   beforeCreate: async data => {
     data.title && ((data.slug = slugify(data.title, { lower: true })), 'slug')
       data.studyField &&
