b203aa41ab3597b5da0924beffa25d01fdd89bb2
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-05-25 19:05:37 +0300

    add slugify and default values for booleans

--- api/life-courses/models/life-courses.js
+++ api/life-courses/models/life-courses.js
@@ -4,5 +4,21 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
+const slugify = require('slugify')
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async data => {
+      data.title && ((data.slug = slugify(data.title, { lower: true })), 'slug')
+        data.studyField &&
+          (data.studyFieldSlug =
+            slugify(data.studyField, { lower: true }) || 'studyFieldSlug')
+    },
+    // beforeUpdate: async (params, data) => {
+    //   data.title && (data.slug = slugify(data.title, { lower: true }) || 'slug')
+    //     data.studyField &&
+    //       (data.studyFieldSlug =
+    //         slugify(data.studyField, { lower: true }) || 'studyFieldSlug')
+    // }
+  }
+};
